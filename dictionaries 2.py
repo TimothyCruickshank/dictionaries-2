a4ed1ec637a33faadf1d8cf71393bdbc58b31cdb
TimothyCruickshank <111780723+TimothyCruickshank@users.noreply.github.com>
2022-11-30 12:46:34 -0500

    first commit

---
+++ dictionaries 2.py
@@ -0,0 +1,11 @@
+sample_1 = {
+    "Rocky" : "Moose",
+    "Mickey" : "Mouse",
+    "Baloo" : "Bear",
+    "Bullwinkle" : "Squirrel",
+}
+
+
+print(sample_1)
+print(sample_1["Rocky"])
+print(sample_1["Baloo"])
